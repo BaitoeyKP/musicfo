c6ec555f4903512a589fd97c55b5763fef87e0eb
BaitoeyKP <kanyarat_toey@hotmail.com>
2024-01-29 17:48:26 +0700

    navbar

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,7 +1,12 @@
 import Navbar from "../components/Navbar";
 
 function Favorite() {
-    return (<div className="bg-base-100 h-screen w-screen"><h1>Fav</h1><Navbar></Navbar></div>)
+    return (
+        <div className="bg-base-100 h-screen w-screen">
+            <Navbar></Navbar>
+            <h1>Fav</h1>
+        </div>
+    )
 }
 
 export default Favorite;
